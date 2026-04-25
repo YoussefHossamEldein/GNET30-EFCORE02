d605843085ce4712ace238c83ce1de7f87d2c9a3
youssef hossam eldein <youssefhossameldeinn@gmail.com>
2026-04-25 21:29:21 +0300

    Add project files.

---
+++ Assignment02EFCore02/Organizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02EFCore02
+{
+    internal class Organizer
+    {
+        public  MyProperty { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public bool IsApplicable { get; set; }
+    }
+}
---
+++ Assignment02EFCore02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment02EFCore02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
